f6d12e6c703ae2b988953e93ca28cb10bfcf4c98
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-04-19 13:18:29 -0500

    utiliza webpack-dev-server

---
+++ webpack-dev-server/index.js
@@ -0,0 +1,4 @@
+import './estilos.css';
+
+document.write('Hola mundo desde Webpack Dev Serve!');
+console.log('Hola mundo!, desde Webpack en un webpack.config');
---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // Aqui van los loaders
+      {
+        // test: que tipo de archivo quiero reconocer
+        // use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader'],
+          // fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    // aqui van los plugins
+    new ExtractTextPlugin("css/styles.css")
+  ]
+}
+
